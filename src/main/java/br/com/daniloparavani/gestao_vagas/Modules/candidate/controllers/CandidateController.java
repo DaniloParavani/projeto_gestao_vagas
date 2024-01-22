a2cdbabe2c05c5a94233097c51f5c0a7fd975491
Danilo Paravani <daniloparavani@yahoo.com.br>
2024-01-22 13:45:48 -0300

    Creating project structure.

---
+++ src/main/java/br/com/daniloparavani/gestao_vagas/Modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,24 @@
+package br.com.daniloparavani.gestao_vagas.Modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.daniloparavani.gestao_vagas.Modules.candidate.CandidateEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+    
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    }
+    
+
+}
