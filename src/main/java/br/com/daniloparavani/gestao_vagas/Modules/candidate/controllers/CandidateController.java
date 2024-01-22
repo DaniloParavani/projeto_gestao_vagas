268dd23abc2de60d5160bc61f6de6163a694ac5d
Danilo Paravani <daniloparavani@yahoo.com.br>
2024-01-22 13:59:47 -0300

    Inserting validation

--- src/main/java/br/com/daniloparavani/gestao_vagas/Modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/daniloparavani/gestao_vagas/Modules/candidate/controllers/CandidateController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.daniloparavani.gestao_vagas.Modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
     
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
